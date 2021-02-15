9ac204517fe6b1c79f08b74adfb94b7e366fd453
lukas-plojhar <57949123+lukas-plojhar@users.noreply.github.com>
2021-02-15 19:42:51 +0100

    DB update

--- DjangoReactEcom/settings.py
+++ DjangoReactEcom/settings.py
@@ -25,20 +25,19 @@ ALLOWED_HOSTS = [
 CORS_ORIGIN_ALLOW_ALL = True
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends
-    #     .postgresql_psycopg2',
-    #     'HOST': 'ec2-54-220-35-19.eu-west-1.compute.amazonaws.com',
-    #     'NAME': 'd56j7rfn4eclsp',
-    #     'USER': 'loerjgdrysicqm',
-    #     'PORT': '5432',
-    #     'PASSWORD': '32fcce2a55cdbe5ea3958b362f0ecf53789490d031eafd6d18e0f10edb9dfe05',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     # 'NAME': BASE_DIR / 'db.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'ec2-54-220-35-19.eu-west-1.compute.amazonaws.com',
+        'NAME': 'd56j7rfn4eclsp',
+        'USER': 'loerjgdrysicqm',
+        'PORT': '5432',
+        'PASSWORD': '32fcce2a55cdbe5ea3958b362f0ecf53789490d031eafd6d18e0f10edb9dfe05',
+    }
 }
 
 # Application definition
